6db44780ccbfb06877dc18417a9c9a99c633f1d6
Ishaqq <ishaq8283@gmail.com>
2022-07-26 16:51:52 +0500

    Update base.java

--- src/main/java/resources/base.java
+++ src/main/java/resources/base.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.BeforeTest;
 
 public class base {
 	public WebDriver driver;
